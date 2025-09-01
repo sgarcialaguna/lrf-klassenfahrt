6f69214a85196e80d95f239addace8a96b90188a
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-01 20:16:29 +0200

    Show selected options

--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -2,37 +2,65 @@ import { Checkbox } from "./ui/checkbox";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 
-export default function Poll() {
+function MonthCheckbox({
+  number,
+  label,
+  months,
+}: {
+  number: number;
+  label: string;
+  months: Array<number>;
+}) {
+  return (
+    <div className="flex items-center gap-3">
+      <Checkbox
+        defaultChecked={months.includes(number)}
+        name="months"
+        id={number.toString()}
+        value={number.toString()}
+      ></Checkbox>
+      <Label htmlFor={number.toString()}>{label}</Label>
+    </div>
+  );
+}
+
+export default function Poll({ months }: { months: Array<number> }) {
   return (
     <>
       <h1 className="text-2xl mb-8">Wann kannst du fahren?</h1>
 
       <form method="post">
         <div className="grid gap-4 grid-rows-3 grid-flow-col">
-          <div className="flex items-center gap-3">
-            <Checkbox name="months" id="10" value="10"></Checkbox>
-            <Label htmlFor="10">Oktober</Label>
-          </div>
-          <div className="flex items-center gap-3">
-            <Checkbox name="months" id="11" value="11"></Checkbox>
-            <Label htmlFor="11">November</Label>
-          </div>
-          <div className="flex items-center gap-3">
-            <Checkbox name="months" id="12" value="12"></Checkbox>
-            <Label htmlFor="12">Dezember</Label>
-          </div>
-          <div className="flex items-center gap-3">
-            <Checkbox name="months" id="1" value="1"></Checkbox>
-            <Label htmlFor="1">Januar 2026</Label>
-          </div>
-          <div className="flex items-center gap-3">
-            <Checkbox name="months" id="2" value="2"></Checkbox>
-            <Label htmlFor="2">Februar 2026</Label>
-          </div>
-          <div className="flex items-center gap-3">
-            <Checkbox name="months" id="3" value="3"></Checkbox>
-            <Label htmlFor="3">März 2026</Label>
-          </div>
+          <MonthCheckbox
+            months={months}
+            number={10}
+            label="Oktober"
+          ></MonthCheckbox>
+          <MonthCheckbox
+            months={months}
+            number={11}
+            label="November"
+          ></MonthCheckbox>
+          <MonthCheckbox
+            months={months}
+            number={12}
+            label="Dezember"
+          ></MonthCheckbox>
+          <MonthCheckbox
+            months={months}
+            number={1}
+            label="Januar 2026"
+          ></MonthCheckbox>
+          <MonthCheckbox
+            months={months}
+            number={2}
+            label="Februar 2026"
+          ></MonthCheckbox>
+          <MonthCheckbox
+            months={months}
+            number={3}
+            label="März 2026"
+          ></MonthCheckbox>
         </div>
 
         <Button type="submit" className="mt-8">
